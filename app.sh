a486b553848502fd3e733ed7e076a357997b943b
Ricardo Padilha <ricardospadilha@gmail.com>
2015-06-11 10:41:35 -0700

    Updated to version 1.42.13

--- app.sh
+++ app.sh
@@ -1,6 +1,6 @@
 ### E2FSPROGS ###
 _build_e2fsprogs() {
-local VERSION="1.42.12"
+local VERSION="1.42.13"
 local FOLDER="e2fsprogs-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
 local URL="http://sourceforge.net/projects/e2fsprogs/files/e2fsprogs/v${VERSION}/${FILE}"
